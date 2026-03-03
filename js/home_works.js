1f14ce0f037e97b454376a0a469fe3a648efb380
bygeto <bysugugeto@gmail.com>
2026-03-03 18:03:26 +0600

    update

--- js/home_works.js
+++ js/home_works.js
@@ -21,21 +21,75 @@ gmailBtn.onclick = () => {
 const block = document.querySelector(".child_block");
 const boxBlock = document.querySelector(".parent_block");
 
-let position = 0;
-let speed = 3;
+let positionX = 0;
+let positionY = 0;
+const speed = 10;
 
-const maxPosition = boxBlock.clientWidth - block.clientWidth;
+const maxX = boxBlock.clientWidth - block.clientWidth;
+const maxY = boxBlock.clientHeight - block.clientHeight;
+
+let direction = "right";
 
 function animate() {
-  position += speed;
-  if (position >= maxPosition) {
-    position = maxPosition;
-    block.style.left = position + "px";
-    return;
+  if (direction === "right") {
+    positionX += speed;
+    if (positionX >= maxX) {
+      positionX = maxX;
+      direction = "down";
+    }
+  } else if (direction === "down") {
+    positionY += speed;
+    if (positionY >= maxY) {
+      positionY = maxY;
+      direction = "left";
+    }
+  } else if (direction === "left") {
+    positionX -= speed;
+    if (positionX <= 0) {
+      positionX = 0;
+      direction = "up";
+    }
+  } else if (direction === "up") {
+    positionY -= speed;
+    if (positionY <= 0) {
+      positionY = 0;
+      direction = "right";
+    }
   }
-  block.style.left = position + "px";
+  block.style.left = `${positionX}px`;
+  block.style.top = `${positionY}px`;
+
   requestAnimationFrame(animate);
 }
+
 requestAnimationFrame(animate);
 
 // СПАСИБО ЗА УРОК! ПОДНЯЛИ НАСТРОЕНИЕ! ❤️
+// DZ-2 ТАЙМЕР                   /// И КСТАТИ ЗАДАНИЕ С КУБОМ ПОЛНОСТЬЮ ГОТОВОЕ СВЕРХУ ^
+
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+const secondsBlock = document.getElementById("seconds");
+
+let time = 0;
+let intervalId = null;
+
+startBtn.onclick = () => {
+  if (intervalId != null) return;
+  intervalId = setInterval(() => {
+    time++;
+    secondsBlock.innerText = time;
+  }, 1000);
+};
+stopBtn.onclick = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+resetBtn.onclick = () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  time = 0;
+  secondsBlock.innerText = time;
+};
