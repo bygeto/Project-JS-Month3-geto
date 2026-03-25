afd5a4455bca05cbba32f5c34d19fdc48a08efe4
bygeto <bysugugeto@gmail.com>
2026-03-25 12:22:47 +0600

    update

--- js/home_works.js
+++ js/home_works.js
@@ -1,135 +1,239 @@
-// DZ-1 @GMAIL.COM 💖
+// // DZ-1 @GMAIL.COM 💖
 
-const gmailInput = document.getElementById("gmail_input");
-const gmailBtn = document.getElementById("gmail_button");
-const gmailResult = document.getElementById("gmail_result");
+// const gmailInput = document.getElementById("gmail_input");
+// const gmailBtn = document.getElementById("gmail_button");
+// const gmailResult = document.getElementById("gmail_result");
 
-const regExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+// const regExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+// gmailBtn.onclick = () => {
+//   if (regExp.test(gmailInput.value)) {
+//     gmailResult.innerText = "GOOD!";
+//     gmailResult.style.color = "green";
+//   } else {
+//     gmailResult.innerText = "ERROR!";
+//     gmailResult.style.color = "red";
+//   }
+// };
+
+// // DZ-2 RED BLOCK 💖
+
+// const block = document.querySelector(".child_block");
+// const boxBlock = document.querySelector(".parent_block");
+
+// let positionX = 0;
+// let positionY = 0;
+// const speed = 10;
+
+// const maxX = boxBlock.clientWidth - block.clientWidth;
+// const maxY = boxBlock.clientHeight - block.clientHeight;
+
+// let direction = "right";
+
+// function animate() {
+//   if (direction === "right") {
+//     positionX += speed;
+//     if (positionX >= maxX) {
+//       positionX = maxX;
+//       direction = "down";
+//     }
+//   } else if (direction === "down") {
+//     positionY += speed;
+//     if (positionY >= maxY) {
+//       positionY = maxY;
+//       direction = "left";
+//     }
+//   } else if (direction === "left") {
+//     positionX -= speed;
+//     if (positionX <= 0) {
+//       positionX = 0;
+//       direction = "up";
+//     }
+//   } else if (direction === "up") {
+//     positionY -= speed;
+//     if (positionY <= 0) {
+//       positionY = 0;
+//       direction = "right";
+//     }
+//   }
+//   block.style.left = `${positionX}px`;
+//   block.style.top = `${positionY}px`;
+
+//   requestAnimationFrame(animate);
+// }
+
+// requestAnimationFrame(animate);
+
+// // СПАСИБО ЗА УРОК! ПОДНЯЛИ НАСТРОЕНИЕ! ❤️
+// // DZ-2 ТАЙМЕР                   /// И КСТАТИ ЗАДАНИЕ С КУБОМ ПОЛНОСТЬЮ ГОТОВОЕ СВЕРХУ ^
+
+// const startBtn = document.getElementById("start");
+// const stopBtn = document.getElementById("stop");
+// const resetBtn = document.getElementById("reset");
+
+// const secondsBlock = document.getElementById("seconds");
+
+// let time = 0;
+// let intervalId = null;
+
+// startBtn.onclick = () => {
+//   if (intervalId != null) return;
+//   intervalId = setInterval(() => {
+//     time++;
+//     secondsBlock.innerText = time;
+//   }, 1000);
+// };
+// stopBtn.onclick = () => {
+//   clearInterval(intervalId);
+//   intervalId = null;
+// };
+// resetBtn.onclick = () => {
+//   clearInterval(intervalId);
+//   intervalId = null;
+//   time = 0;
+//   secondsBlock.innerText = time;
+// };
+
+// // DZ-4
+
+// const request = new XMLHttpRequest();
+
+// request.open("GET", "../data/characters.json");
+
+// request.send();
+
+// request.onload = () => {
+//   if (request.status === 200) {
+//     const data = JSON.parse(request.response);
+
+//     const container = document.querySelector(".characters-list");
+
+//     data.forEach((character) => {
+//       const card = document.createElement("div");
+//       card.className = "character-card";
+
+//       card.innerHTML = `
+//         <img src="${character.photo}" alt="${character.name}">
+//         <h3>${character.name}</h3>
+//         <p>Age: ${character.age}</p>
+//       `;
+
+//       container.append(card);
+//     });
+//   }
+// };
+
+// const requestAny = new XMLHttpRequest();
+
+// requestAny.open("GET", "../data/info.json");
+
+// requestAny.send();
+
+// requestAny.onload = () => {
+//   const data = JSON.parse(requestAny.response);
+//   console.log(data);
+// };
+
+// ===== GMAIL =====
+const gmailInput = document.querySelector("#gmail_input");
+const gmailBtn = document.querySelector("#gmail_button");
+const gmailResult = document.querySelector("#gmail_result");
+
+const gmailRegExp = /^[a-z0-9._%+-]+@gmail\.com$/i;
 
 gmailBtn.onclick = () => {
-  if (regExp.test(gmailInput.value)) {
-    gmailResult.innerText = "GOOD!";
+  if (gmailRegExp.test(gmailInput.value)) {
+    gmailResult.innerText = "VALID";
     gmailResult.style.color = "green";
   } else {
-    gmailResult.innerText = "ERROR!";
+    gmailResult.innerText = "INVALID";
     gmailResult.style.color = "red";
   }
 };
 
-// DZ-2 RED BLOCK 💖
-
+// ===== RED BLOCK =====
 const block = document.querySelector(".child_block");
-const boxBlock = document.querySelector(".parent_block");
+const parent = document.querySelector(".parent_block");
 
-let positionX = 0;
-let positionY = 0;
-const speed = 10;
-
-const maxX = boxBlock.clientWidth - block.clientWidth;
-const maxY = boxBlock.clientHeight - block.clientHeight;
-
+let x = 0;
+let y = 0;
 let direction = "right";
 
-function animate() {
-  if (direction === "right") {
-    positionX += speed;
-    if (positionX >= maxX) {
-      positionX = maxX;
-      direction = "down";
-    }
-  } else if (direction === "down") {
-    positionY += speed;
-    if (positionY >= maxY) {
-      positionY = maxY;
-      direction = "left";
-    }
-  } else if (direction === "left") {
-    positionX -= speed;
-    if (positionX <= 0) {
-      positionX = 0;
-      direction = "up";
-    }
-  } else if (direction === "up") {
-    positionY -= speed;
-    if (positionY <= 0) {
-      positionY = 0;
-      direction = "right";
-    }
-  }
-  block.style.left = `${positionX}px`;
-  block.style.top = `${positionY}px`;
+const maxX = parent.clientWidth - block.clientWidth;
+const maxY = parent.clientHeight - block.clientHeight;
 
-  requestAnimationFrame(animate);
+function move() {
+  if (direction === "right") {
+    x += 5;
+    if (x >= maxX) direction = "down";
+  } else if (direction === "down") {
+    y += 5;
+    if (y >= maxY) direction = "left";
+  } else if (direction === "left") {
+    x -= 5;
+    if (x <= 0) direction = "up";
+  } else if (direction === "up") {
+    y -= 5;
+    if (y <= 0) direction = "right";
+  }
+
+  block.style.left = x + "px";
+  block.style.top = y + "px";
+
+  requestAnimationFrame(move);
 }
 
-requestAnimationFrame(animate);
+move();
 
-// СПАСИБО ЗА УРОК! ПОДНЯЛИ НАСТРОЕНИЕ! ❤️
-// DZ-2 ТАЙМЕР                   /// И КСТАТИ ЗАДАНИЕ С КУБОМ ПОЛНОСТЬЮ ГОТОВОЕ СВЕРХУ ^
-
-const startBtn = document.getElementById("start");
-const stopBtn = document.getElementById("stop");
-const resetBtn = document.getElementById("reset");
-
-const secondsBlock = document.getElementById("seconds");
+// ===== STOPWATCH =====
+const seconds = document.querySelector("#seconds");
+const start = document.querySelector("#start");
+const stop = document.querySelector("#stop");
+const reset = document.querySelector("#reset");
 
 let time = 0;
-let intervalId = null;
+let interval = null;
 
-startBtn.onclick = () => {
-  if (intervalId != null) return;
-  intervalId = setInterval(() => {
+start.onclick = () => {
+  if (interval) return;
+
+  interval = setInterval(() => {
     time++;
-    secondsBlock.innerText = time;
+    seconds.innerText = time;
   }, 1000);
 };
-stopBtn.onclick = () => {
-  clearInterval(intervalId);
-  intervalId = null;
+
+stop.onclick = () => {
+  clearInterval(interval);
+  interval = null;
 };
-resetBtn.onclick = () => {
-  clearInterval(intervalId);
-  intervalId = null;
+
+reset.onclick = () => {
+  clearInterval(interval);
+  interval = null;
   time = 0;
-  secondsBlock.innerText = time;
+  seconds.innerText = 0;
 };
 
-// DZ-4
-
+// ===== XHR CHARACTERS =====
 const request = new XMLHttpRequest();
-
 request.open("GET", "../data/characters.json");
-
 request.send();
 
 request.onload = () => {
-  if (request.status === 200) {
-    const data = JSON.parse(request.response);
+  const data = JSON.parse(request.response);
+  const container = document.querySelector(".characters-list");
 
-    const container = document.querySelector(".characters-list");
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "character-card";
 
-    data.forEach((character) => {
-      const card = document.createElement("div");
-      card.className = "character-card";
+    card.innerHTML = `
+      <img src="${item.photo}">
+      <h3>${item.name}</h3>
+      <p>${item.age}</p>
+    `;
 
-      card.innerHTML = `
-        <img src="${character.photo}" alt="${character.name}">
-        <h3>${character.name}</h3>
-        <p>Age: ${character.age}</p>
-      `;
-
-      container.append(card);
-    });
-  }
-};
-
-const requestAny = new XMLHttpRequest();
-
-requestAny.open("GET", "../data/info.json");
-
-requestAny.send();
-
-requestAny.onload = () => {
-  const data = JSON.parse(requestAny.response);
-  console.log(data);
+    container.append(card);
+  });
 };
--- js/lesson.js
+++ js/lesson.js
@@ -14,5 +14,101 @@ phoneButton.onclick = () => {
   }
 };
 
-////////////////////////////////////
+// ===== TAB =====
+const tabs = document.querySelectorAll(".tab_content_block");
+const items = document.querySelectorAll(".tab_content_item");
 
+let index = 0;
+
+function hideTabs() {
+  tabs.forEach((t) => (t.style.display = "none"));
+  items.forEach((i) => i.classList.remove("tab_content_item_active"));
+}
+
+function showTab(i) {
+  tabs[i].style.display = "block";
+  items[i].classList.add("tab_content_item_active");
+}
+
+hideTabs();
+showTab(index);
+
+setInterval(() => {
+  index++;
+  if (index >= tabs.length) index = 0;
+  hideTabs();
+  showTab(index);
+}, 3000);
+
+// ===== CONVERTER =====
+const som = document.querySelector("#som");
+const usd = document.querySelector("#usd");
+
+const rate = 89;
+
+som.oninput = () => {
+  if (!som.value) return (usd.value = "");
+  usd.value = (som.value / rate).toFixed(2);
+};
+
+usd.oninput = () => {
+  if (!usd.value) return (som.value = "");
+  som.value = (usd.value * rate).toFixed(2);
+};
+
+// ===== CARD SWITCHER =====
+const card = document.querySelector(".card");
+const prev = document.querySelector("#btn-prev");
+const next = document.querySelector("#btn-next");
+
+let id = 1;
+const max = 200;
+
+function getCard(id) {
+  fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    .then((r) => r.json())
+    .then((data) => {
+      card.innerHTML = `
+        <p>${data.id}</p>
+        <h3>${data.title}</h3>
+        <span>${data.completed}</span>
+      `;
+    });
+}
+
+getCard(id);
+
+next.onclick = () => {
+  id++;
+  if (id > max) id = 1;
+  getCard(id);
+};
+
+prev.onclick = () => {
+  id--;
+  if (id < 1) id = max;
+  getCard(id);
+};
+
+// ===== WEATHER =====
+const input = document.querySelector("#searchInput");
+const btn = document.querySelector("#search");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+
+const API = "291aa3950880603684e43c6cc36aed88";
+
+btn.onclick = () => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${API}&units=metric&lang=ru`,
+  )
+    .then((r) => r.json())
+    .then((data) => {
+      city.innerText = data.name;
+      temp.innerText = Math.round(data.main.temp) + "°C";
+    })
+    .catch(() => {
+      city.innerText = "Ошибка";
+      temp.innerText = "";
+    });
+};
